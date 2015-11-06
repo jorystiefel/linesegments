fd2a1ddb7cc98ec91310b785c46f96eda7b567cf
JLS <jorystiefel@me.com>
2015-11-06 10:59:56 -0500

    Initial Commit

---
+++ Swift Geometry/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swift Geometry
+//
+//  Created by Jory Stiefel on 11/6/15.
+//  Copyright © 2015 Jory Stiefel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swift GeometryTests/Swift_GeometryTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift_GeometryTests.swift
+//  Swift GeometryTests
+//
+//  Created by Jory Stiefel on 11/6/15.
+//  Copyright © 2015 Jory Stiefel. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_Geometry
+
+class Swift_GeometryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
